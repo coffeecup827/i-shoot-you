5edbed2d89e37ea77da1e2db2c1a4753ea94e734
CoffeeCup <CoffeeCup827@gmail.com>
2026-07-16 23:55:22 +0530

    First animation added

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -1,7 +1,12 @@
 using Godot;
+using System;
 
 public partial class Player : Node2D
 {
+
+	[Export] 
+	private Node2D _playerShip;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -10,5 +15,6 @@ public partial class Player : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		_playerShip.Rotation += 1 * (float)delta;
 	}
 }
