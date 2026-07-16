f7b5ea57a3bfb711e51d8ea1d8ff2c329ec0fc46
CoffeeCup <CoffeeCup827@gmail.com>
2026-07-17 04:22:42 +0530

    Adds Dynamic Meteor Textures

--- core/PathsWithRandom.cs
+++
@@ -1,6 +0,0 @@
-using iShootYou;
-
-public static class PathsWithRandom
-{
-    public static readonly string MeteorsPath = "res://assets/Meteor/meteor.tscn";
-}
--- scenes/meteor/Meteor.cs
+++ scenes/meteor/Meteor.cs
@@ -7,12 +7,17 @@ public partial class Meteor : Area2D
 	[Export]
 	private float meteorDefaultSpeed;
 
+	[Export]
+	private Sprite2D _meteorSprite;
+
 	int meteorXDirection = 0;
 	float meteorSpeed = 0;
 	float meteorSpeedMultiplier = (float)GD.RandRange(1f, 3f);
 
 	public override void _Ready()
 	{
+		_meteorSprite.Texture = GD.Load<Texture2D>(Paths.GetMeteorsPath(Random.Shared.Next(1, 24)));
+
 		Position = new Vector2(GD.Randf() * GetViewportRect().Size.X, Random.Shared.Next(-150, -50));
 		meteorXDirection = Random.Shared.Next(-1, 1);
 		meteorSpeed = meteorDefaultSpeed * meteorSpeedMultiplier;
@@ -21,7 +26,7 @@ public partial class Meteor : Area2D
 	public override void _Process(double delta)
 	{
 		Position += new Vector2(meteorXDirection, 1) * (float)delta * meteorSpeed;
-		//RotationDegrees += (float)delta * 100;
+		RotationDegrees += (float)delta * 100;
 	}
 
 	private void OnBodyEntered(Node2D body)
