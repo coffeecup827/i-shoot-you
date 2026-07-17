7393a8a33168914ef08e883ad8db1ef17c0d0913
CoffeeCup <CoffeeCup827@gmail.com>
2026-07-17 05:40:32 +0530

    Shoots Laser

--- scenes/laser/Laser.cs
+++ scenes/laser/Laser.cs
@@ -3,4 +3,10 @@ using System;
 
 public partial class Laser : Area2D
 {
+    [Export]
+    private int laserSpeed;
+    public override void _Process(double delta)
+    {
+        Position += new Vector2(0, -1) * (float)delta * laserSpeed;
+    }
 }
