8e90453f0e928a2f034dcfc138ff1882faf44763
CoffeeCup <CoffeeCup827@gmail.com>
2026-07-18 06:23:09 +0530

    feat: Audio Files List Auto Generated

---
+++ core/audio/AudioCue.cs
@@ -0,0 +1,16 @@
+// ========================================== 
+// AUTO-GENERATED AT BUILD TIME. DO NOT EDIT.
+// ========================================== 
+public static class AudioCue
+{
+    public const string ExplosionSound1 = "explosion_sound_1";
+    public const string ExplosionSound2 = "explosion_sound_2";
+    public const string ExplosionSound3 = "explosion_sound_3";
+    public const string LaserSound1 = "laser_sound_1";
+    public const string LaserSound2 = "laser_sound_2";
+    public const string LaserSound3 = "laser_sound_3";
+    public const string LaserSound4 = "laser_sound_4";
+    public const string MusicBgmFast = "music_bgm_fast";
+    public const string MusicBgmSlow = "music_bgm_slow";
+    public const string PlayerPlayerHit = "player_player_hit";
+}
--- tools/AudioInitialiser.cs
+++ tools/AudioInitialiser.cs
@@ -1,14 +1,59 @@
 using System;
+using System.Text;
 
 namespace Tools;
 
 public static class AudioInitialiser
 {
+    private const string audioFolderPath = ".\\assets\\audio";
+    private const string outputFilePath = ".\\core\\audio\\AudioCue.cs";
+
+    private static readonly HashSet<string> audioFormats = [".wav", ".mp3"];
+
     public static void Run()
     {
         Console.WriteLine("[TOOL] Running Audio Initialiser...");
 
+        HashSet<string> sounds = new HashSet<string>();
+        var audioSubFolders = Directory.GetDirectories(audioFolderPath).ToList();
+
+        audioSubFolders.ForEach(subfolder => AddSound(subfolder, sounds));
+        Console.WriteLine(string.Join(", ", sounds));
+
+        var sb = new StringBuilder();
+        sb.AppendLine("// ========================================== ");
+        sb.AppendLine("// AUTO-GENERATED AT BUILD TIME. DO NOT EDIT.");
+        sb.AppendLine("// ========================================== ");
+        sb.AppendLine("public static class AudioCue");
+        sb.AppendLine("{");
+        foreach (var key in sounds)
+        {
+            string pascalIdentifier = ToPascalCase(key);
+            sb.AppendLine($"    public const string {pascalIdentifier} = \"{key}\";");
+        }
+        sb.AppendLine("}");
+
+        File.WriteAllText(outputFilePath, sb.ToString());
         
-        Console.WriteLine("[TOOL] Audio Initialiser Success");
+        Console.WriteLine("[TOOL] Audio Initialiser ");
+    }
+
+    private static void AddSound(string subfolder, HashSet<string> sounds)
+    {
+        var validAudioFiles = Directory.GetFiles(subfolder).Where(x => audioFormats.Contains(Path.GetExtension(x)));
+        foreach (var audioFile in validAudioFiles)
+        {
+            sounds.Add($"{Path.GetFileName(Path.GetDirectoryName(audioFile))}_{Path.GetFileNameWithoutExtension(audioFile)}".ToLower());
+        }
+    }
+
+    private static string ToPascalCase(string str)
+    {
+        var words = str.Split("_", StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+        }
+        return string.Join("", words);
     }
 }
